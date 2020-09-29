eb6d0a509bdedee93cd34115999900096ed4ed18
Christian Augusto Martins dos Santos Barros <christianbetta@gmail.com>
2020-09-29 19:06:23 -0300

    feat: Mudando MER para facilitar inserção e remoção de pessoas em uma fila

--- digital-queue-software/docs/assets/mysql/1-create-database.sql
+++ digital-queue-software/docs/assets/mysql/1-create-database.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS `digital-queue-software`.`users` (
     `documentType` VARCHAR(50) NOT NULL,
     `nacionalidade` VARCHAR(75) NOT NULL,
     `registerDate` DATETIME NOT NULL,
-    `roleType` ENUM('regular', 'admin', 'master') NOT NULL,
+    `roleType` ENUM('admin', 'master') NOT NULL,
 
     PRIMARY KEY (`id`),
     UNIQUE KEY (`email`),
@@ -26,21 +26,50 @@ CREATE TABLE IF NOT EXISTS `digital-queue-software`.`digital_queues` (
     `name` VARCHAR(100) NOT NULL,
     `slug` VARCHAR(100) NOT NULL,
     `creationDate` DATETIME NOT NULL,
-    `isActive` TINYINT(1) NOT NULL,
+    `isActive` TINYINT(1) UNSIGNED NOT NULL,
 
     PRIMARY KEY (`id`),
     UNIQUE KEY (`slug`)
 );
 
-CREATE TABLE IF NOT EXISTS `digital-queue-software`.`digital_queues_users` (
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `userId` BIGINT UNSIGNED NOT NULL,
-    `digitalQueueId` BIGINT UNSIGNED NOT NULL,
-    `attended` TINYINT(1) NOT NULL,
-    `transportId` TINYINT(1) NOT NULL,
+CREATE TABLE IF NOT EXISTS `digital-queue-software`.`transports` (
+    `id` TINYINT(1) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
 
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`userId`) REFERENCES `users` (`id`),
+    UNIQUE KEY (`name`)
+);
+
+CREATE TABLE IF NOT EXISTS `digital-queue-software`.`digital_queues_users` (
+    `digitalQueueId` BIGINT UNSIGNED NOT NULL,
+    `document` VARCHAR(50) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(128) NOT NULL,
+    `telephone` VARCHAR(30) NOT NULL,
+    `documentType` VARCHAR(50) NOT NULL,
+    `nacionalidade` VARCHAR(75) NOT NULL,
+    `registerDate` DATETIME NOT NULL,
+    `attended` TINYINT(1) UNSIGNED NOT NULL,
+    `transportId` TINYINT(1) UNSIGNED NOT NULL,
+
+    PRIMARY KEY (`digitalQueueId`, `document`),
     FOREIGN KEY (`digitalQueueId`) REFERENCES `digital_queues` (`id`),
     FOREIGN KEY (`transportId`) REFERENCES `transports` (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `digital-queue-software`.`digital_queues_transports` (
+    `id` TINYINT(1) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `digitalQueueId` BIGINT UNSIGNED NOT NULL,
+    `transportId` TINYINT(1) UNSIGNED NOT NULL,
+
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`digitalQueueId`) REFERENCES `digital_queues` (`id`),
+    FOREIGN KEY (`transportId`) REFERENCES `transports` (`id`)
+);
+
+INSERT INTO `digital-queue-software`.`transports`
+    (`name`)
+VALUES
+    ('A pé'),
+    ('carro');
--- digital-queue-software/docs/assets/mysql/examples.sql
+++ digital-queue-software/docs/assets/mysql/examples.sql
@@ -6,7 +6,18 @@ VALUES
     ('Christian Augusto Martins dos Santos Barros', 'christianbetta@gmail.com', 'fa585d89c851dd338a70dcf535aa2a92fee7836dd6aff1226583e88e0996293f16bc009c652826e0fc5c706695a03cddce372f139eff4d13959da6f1f5d3eabe', '(11) 99311-8482', '508.569.378-78', 'cpf', 'Brasil', NOW(), 'master');
 
 
-INSERT INTO `digital-queue-software`.`digital_queue`
-    (`name`, `slug`, `creationDate`, `isActive`, `transportsAllowed`)
+INSERT INTO `digital-queue-software`.`digital_queues`
+    (`name`, `slug`, `creationDate`, `isActive`)
 VALUES
-    ('Fila Vacina 1', 'fila-vacina-1', NOW(), 1, 'A pé|carro');
+    ('Fila Vacina 1', 'fila-vacina-1', NOW(), 1);
+
+
+INSERT INTO `digital-queue-software`.`digital_queues_transports`
+    (`digitalQueueId`, `transportId`)
+VALUES
+    (1, 1);
+
+INSERT INTO `digital-queue-software`.`digital_queues_transports`
+    (`digitalQueueId`, `transportId`)
+VALUES
+    (1, 2);
