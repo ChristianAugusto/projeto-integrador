f10116845f91bc9c8e7c16b6f573a257d62863fc
Christian Augusto Martins dos Santos Barros <christianbetta@gmail.com>
2020-11-21 18:07:47 -0300

    feat(/admin/filas): Iniciando UI para editar e deletar fila

--- digital-queue-software/website/js/modules/pvt/admin-digital-queues/mount-page.js
+++ digital-queue-software/website/js/modules/pvt/admin-digital-queues/mount-page.js
@@ -37,18 +37,95 @@ async function getDigitalQueues() {
     return [];
 }
 
+
+/**
+    * @param {HTMLElement} digitalQueueItem
+*/
+function openDigitalQueueItemPanel(digitalQueueItem) {
+    digitalQueueItem.classList.add('panel-open');
+}
+
+
+/**
+    * @param {HTMLElement} digitalQueueItem
+*/
+function closeDigitalQueueItemPanel(digitalQueueItem) {
+    digitalQueueItem.classList.remove('panel-open');
+}
+
+
+function editDigitalQueue() {
+    console.log('Editar fila');
+}
+
+
+function deleteDigitalQueue() {
+    console.log('Deletar fila');
+}
+
+
 async function buildDigitalQueuesList() {
     const digitalQueues = await getDigitalQueues();
 
-    El.digitalQueuesList.innerHTML = digitalQueues.data.reduce(function(acc, digitalQueue) {
-        return acc + `
-            <li class="pvt-admin__queues__item">
-                <p>${digitalQueue.name}</p>
-                <a href="/admin/filas/${digitalQueue.id}">Link Privado</a>
-                <a href="/filas/${digitalQueue.id}">Link Público</a>
-            </li>
+    digitalQueues.data.forEach(function(digitalQueue) {
+        const digitalQueueItem = document.createElement('li');
+        digitalQueueItem.classList.add('pvt-admin__digital-queues__item');
+
+        const digitalQueueItemVisor = document.createElement('div');
+        digitalQueueItemVisor.classList.add('pvt-admin__digital-queues__item-visor');
+        digitalQueueItemVisor.innerHTML = `
+            <p>${digitalQueue.name}</p>
+            <a href="/admin/filas/${digitalQueue.id}">Link Privado</a>
+            <a href="/filas/${digitalQueue.id}">Link Público</a>
         `;
-    }, '');
+
+        const openPanelButton = document.createElement('button');
+        openPanelButton.setAttribute('type', 'button');
+        openPanelButton.classList.add('reset');
+        openPanelButton.classList.add('pvt-admin__digital-queues__item-open-panel-button');
+        openPanelButton.onclick = openDigitalQueueItemPanel.bind(null, digitalQueueItem);
+        openPanelButton.textContent = 'Abrir painel';
+
+        const closePanelButton = document.createElement('button');
+        closePanelButton.setAttribute('type', 'button');
+        closePanelButton.classList.add('reset');
+        closePanelButton.classList.add('pvt-admin__digital-queues__item-close-panel-button');
+        closePanelButton.onclick = closeDigitalQueueItemPanel.bind(null, digitalQueueItem);
+        closePanelButton.textContent = 'Fechar painel';
+
+        const digitalQueueItemPanel = document.createElement('div');
+        digitalQueueItemPanel.classList.add('pvt-admin__digital-queues__item-panel');
+
+        const digitalQueueItemPanelActions = document.createElement('div');
+        digitalQueueItemPanelActions.classList.add('pvt-admin__digital-queues__item-panel-actions');
+
+        const editDigitalQueueButton = document.createElement('button');
+        editDigitalQueueButton.setAttribute('type', 'button');
+        editDigitalQueueButton.classList.add('reset');
+        editDigitalQueueButton.classList.add('pvt-admin__digital-queues__item-edit-digital-queue-button');
+        editDigitalQueueButton.onclick = editDigitalQueue;
+        editDigitalQueueButton.textContent = 'Editar fila digital';
+
+        const deleteDigitalQueueButton = document.createElement('button');
+        deleteDigitalQueueButton.setAttribute('type', 'button');
+        deleteDigitalQueueButton.classList.add('reset');
+        deleteDigitalQueueButton.classList.add('pvt-admin__digital-queues__item-delete-digital-queue-button');
+        deleteDigitalQueueButton.onclick = deleteDigitalQueue;
+        deleteDigitalQueueButton.textContent = 'Deletar fila digital';
+
+        digitalQueueItemPanelActions.appendChild(closePanelButton);
+        digitalQueueItemPanelActions.appendChild(editDigitalQueueButton);
+        digitalQueueItemPanelActions.appendChild(deleteDigitalQueueButton);
+
+        digitalQueueItemPanel.appendChild(openPanelButton);
+        digitalQueueItemPanel.appendChild(digitalQueueItemPanelActions);
+
+
+        digitalQueueItem.appendChild(digitalQueueItemVisor);
+        digitalQueueItem.appendChild(digitalQueueItemPanel);
+
+        El.digitalQueuesList.appendChild(digitalQueueItem);
+    });
 
     El.pageDisplay.innerHTML = controller.page;
 
@@ -71,6 +148,7 @@ async function buildDigitalQueuesList() {
 
     window.location.hash = `#?page=${controller.page}`;
 }
+
 
 function setEvents() {
     El.nextPage.onclick = function() {
